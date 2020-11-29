20f326e690c2c8cc7d0bfe2b0624841202b84c5b
LancelotDabert <71867586+LancelotDabert@users.noreply.github.com>
2020-11-29 21:35:22 +0100

    Update tab.c

--- exercice 1/tab.c
+++ exercice 1/tab.c
@@ -3,29 +3,29 @@
 #include "tab.h"
 
 #define TAB2SIZE 100
-#define TAILLEAJOUT 50
+#define TAILLEAJOUT 50 // definition de constante gloables
 
 int initTab(int* tab, int size);
-int afficheTab(int* tab, int size, int nbElts);
+int afficheTab(int* tab, int size, int nbElts);  // declarations de fonctions 
 
 
 int main()
 {
-	int myTab1[10] = { 12 };
+	int myTab1[10] = { 12 }; // creation d'unn tableau fixe d entiers
 
 
 
-	int* myTab2 = NULL;
-	int tabsize = TAB2SIZE;
-	myTab2 = (int*)malloc(TAB2SIZE * sizeof(int));
-	if (myTab2 != NULL) { initTab(myTab2, tabsize); }
-	else { printf("mémoire insuffisante"); return(-1); }
-	for (int i = 1; i <= 20; i++)
+	int* myTab2 = NULL; // initialisation d un pointeur 
+	int tabsize = TAB2SIZE; // taille du tableau
+	myTab2 = (int*)malloc(TAB2SIZE * sizeof(int)); // allocation de la memoire 
+	if (myTab2 != NULL) { initTab(myTab2, tabsize); } // si allocation reussi on initialise le tableau de zeros
+	else { printf("mÃ©moire insuffisante"); return(-1); } // sinon on renvoie un message d'erreur
+	for (int i = 1; i <= 20; i++) // boucle de remplissage des 20 premieres places du tableau
 	{
-		*(myTab2 + i - 1) = i;
+		*(myTab2 + i - 1) = i; // avec des entiers de 1 a 20 croissants
 	}
-	afficheTab(myTab2, 20, 20);
+	afficheTab(myTab2, 20, 20); // affichage des 20 premieres valeurs du tableau
 
-	free(myTab2);
+	free(myTab2); // liberation de la memoire allouee au tableau 
 
 }
